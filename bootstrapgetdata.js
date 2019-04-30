54fc5c7f76e1b02e7c0435cbe311467df5821a2c
Tyler Redman <tredman@una.edu>
2019-04-30 05:38:55 +0000

    functioning database insert

--- bootstrapgetdata.js
+++ bootstrapgetdata.js
@@ -1,10 +1,11 @@
-/* global $PORT */
+/* global $ */
 
 const express = require('express');
 const app = express();
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const url = "mongodb://127.0.0.1/gradtracker";
 
 app.listen(8080, () => console.log('Server Ready'));
 app.use(bodyParser.json());
@@ -12,120 +13,152 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
-
-app.get('/api/datatable', function(request, response) {
-	// this is the return from the mongo
-	// let mongoarray = db.collection('cars').find({type: "truck"}).toArray();
-	//   each document would look like: {field1: string, field2: int, field3: double, .... };
-	//     mongo array would be array of those docs
-	// let dataSet = [];
-	// for(let i = 0, i < mongoarray.length; i++) {
-	// let fieldArr = [];
-	// fieldArr.push(mongoarray[i].field1.toString()); 
-	// fieldArr.push(mongoarray[i].field2.toString()); 
-	// fieldArr.push(mongoarray[i].field3.toString()); 
-	// fieldArr.push(mongoarray[i].field4.toString()); 
-	// fieldArr.push(mongoarray[i].field5.toString()); 
-	// fieldArr.push(mongoarray[i].field6.toString()); 
-	// dataSet.push(fieldArr);
-	//}
-	let dataSet = [
-		["Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800"],
-		["Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750"],
-		["Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000"],
-		["Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224", "2012/03/29", "$433,060"],
-		["Airi Satou", "Accountant", "Tokyo", "5407", "2008/11/28", "$162,700"],
-		["Brielle Williamson", "Integration Specialist", "New York", "4804", "2012/12/02", "$372,000"],
-		["Herrod Chandler", "Sales Assistant", "San Francisco", "9608", "2012/08/06", "$137,500"],
-		["Rhona Davidson", "Integration Specialist", "Tokyo", "6200", "2010/10/14", "$327,900"],
-		["Colleen Hurst", "Javascript Developer", "San Francisco", "2360", "2009/09/15", "$205,500"],
-		["Sonya Frost", "Software Engineer", "Edinburgh", "1667", "2008/12/13", "$103,600"],
-		["Jena Gaines", "Office Manager", "London", "3814", "2008/12/19", "$90,560"],
-		["Quinn Flynn", "Support Lead", "Edinburgh", "9497", "2013/03/03", "$342,000"],
-		["Charde Marshall", "Regional Director", "San Francisco", "6741", "2008/10/16", "$470,600"],
-		["Haley Kennedy", "Senior Marketing Designer", "London", "3597", "2012/12/18", "$313,500"],
-		["Tatyana Fitzpatrick", "Regional Director", "London", "1965", "2010/03/17", "$385,750"],
-		["Michael Silva", "Marketing Designer", "London", "1581", "2012/11/27", "$198,500"],
-		["Paul Byrd", "Chief Financial Officer (CFO)", "New York", "3059", "2010/06/09", "$725,000"],
-		["Gloria Little", "Systems Administrator", "New York", "1721", "2009/04/10", "$237,500"],
-		["Bradley Greer", "Software Engineer", "London", "2558", "2012/10/13", "$132,000"],
-		["Dai Rios", "Personnel Lead", "Edinburgh", "2290", "2012/09/26", "$217,500"],
-		["Jenette Caldwell", "Development Lead", "New York", "1937", "2011/09/03", "$345,000"],
-		["Yuri Berry", "Chief Marketing Officer (CMO)", "New York", "6154", "2009/06/25", "$675,000"],
-		["Caesar Vance", "Pre-Sales Support", "New York", "8330", "2011/12/12", "$106,450"],
-		["Doris Wilder", "Sales Assistant", "Sidney", "3023", "2010/09/20", "$85,600"],
-		["Angelica Ramos", "Chief Executive Officer (CEO)", "London", "5797", "2009/10/09", "$1,200,000"],
-		["Gavin Joyce", "Developer", "Edinburgh", "8822", "2010/12/22", "$92,575"],
-		["Jennifer Chang", "Regional Director", "Singapore", "9239", "2010/11/14", "$357,650"],
-		["Brenden Wagner", "Software Engineer", "San Francisco", "1314", "2011/06/07", "$206,850"],
-		["Fiona Green", "Chief Operating Officer (COO)", "San Francisco", "2947", "2010/03/11", "$850,000"],
-		["Shou Itou", "Regional Marketing", "Tokyo", "8899", "2011/08/14", "$163,000"],
-		["Michelle House", "Integration Specialist", "Sidney", "2769", "2011/06/02", "$95,400"],
-		["Suki Burks", "Developer", "London", "6832", "2009/10/22", "$114,500"],
-		["Prescott Bartlett", "Technical Author", "London", "3606", "2011/05/07", "$145,000"],
-		["Gavin Cortez", "Team Leader", "San Francisco", "2860", "2008/10/26", "$235,500"],
-		["Martena Mccray", "Post-Sales support", "Edinburgh", "8240", "2011/03/09", "$324,050"],
-		["Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675"]
-	];
-
-	response.end(JSON.stringify(dataSet));
-	/*response.writeHead(200);
-	response.write('<html><body><p>Please specify an animal in the URL path</p></body></html>');
-	response.end();*/
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-MongoClient.connect('mongodb://localhost:27017', (err, client) => {
-	if (err) throw err;
-	const db = client.db('gradtracker');
-	app.get('/students', (req, res) => {
-
-		db.collection('students').find().toArray((err, result) => {
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
 			if (err) throw err;
-			console.log(result);
-			res.json(result);
-		});
+			var dbo = db.db("gradtracker");
+			var collection = dbo.collection('students');
 
-	})
+			app.get('/api/datatable', function(request, response) {
+
+				collection.find({}).toArray(function(err, results) {
+					if (!err) {
+						let dataSet = [];
+						for (let i = 0; i < results.length; i++) {
+							let fieldArr = [];
+							fieldArr.push(results[i].name);
+							fieldArr.push(results[i].position);
+							fieldArr.push(results[i].location);
+							fieldArr.push(results[i].lnumber);
+							fieldArr.push(results[i].graddate);
+							fieldArr.push(results[i].salary);
+							dataSet.push(fieldArr);
+						}
+						//console.log(dataSet);
+						response.end(JSON.stringify(dataSet));
+					}
+					else {
+						response.send(err);
+					}
+					
+					/*response.writeHead(200);
+					response.write('<html><body><p>Please specify an animal in the URL path</p></body></html>');
+					response.end();*/
+
+					});
+			});
+			// 	let dataSet = [
+			// 			["Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800"],
+			// 			["Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750"],
+			// 			["Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000"],
+			// 			["Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224", "2012/03/29", "$433,060"],
+			// 			["Airi Satou", "Accountant", "Tokyo", "5407", "2008/11/28", "$162,700"],
+			// 			["Brielle Williamson", "Integration Specialist", "New York", "4804", "2012/12/02", "$372,000"],
+			// 			["Herrod Chandler", "Sales Assistant", "San Francisco", "9608", "2012/08/06", "$137,500"],
+			// 			["Rhona Davidson", "Integration Specialist", "Tokyo", "6200", "2010/10/14", "$327,900"],
+			// 			["Colleen Hurst", "Javascript Developer", "San Francisco", "2360", "2009/09/15", "$205,500"],
+			// 			["Sonya Frost", "Software Engineer", "Edinburgh", "1667", "2008/12/13", "$103,600"],
+			// 			["Jena Gaines", "Office Manager", "London", "3814", "2008/12/19", "$90,560"],
+			// 			["Quinn Flynn", "Support Lead", "Edinburgh", "9497", "2013/03/03", "$342,000"],
+			// 			["Charde Marshall", "Regional Director", "San Francisco", "6741", "2008/10/16", "$470,600"],
+			// 			["Haley Kennedy", "Senior Marketing Designer", "London", "3597", "2012/12/18", "$313,500"],
+			// 			["Tatyana Fitzpatrick", "Regional Director", "London", "1965", "2010/03/17", "$385,750"],
+			// 			["Michael Silva", "Marketing Designer", "London", "1581", "2012/11/27", "$198,500"],
+			// 			["Paul Byrd", "Chief Financial Officer (CFO)", "New York", "3059", "2010/06/09", "$725,000"],
+			// 			["Gloria Little", "Systems Administrator", "New York", "1721", "2009/04/10", "$237,500"],
+			// 			["Bradley Greer", "Software Engineer", "London", "2558", "2012/10/13", "$132,000"],
+			// 			["Dai Rios", "Personnel Lead", "Edinburgh", "2290", "2012/09/26", "$217,500"],
+			// 			["Jenette Caldwell", "Development Lead", "New York", "1937", "2011/09/03", "$345,000"],
+			// 			["Yuri Berry", "Chief Marketing Officer (CMO)", "New York", "6154", "2009/06/25", "$675,000"],
+			// 			["Caesar Vance", "Pre-Sales Support", "New York", "8330", "2011/12/12", "$106,450"],
+			// 			["Doris Wilder", "Sales Assistant", "Sidney", "3023", "2010/09/20", "$85,600"],
+			// 			["Angelica Ramos", "Chief Executive Officer (CEO)", "London", "5797", "2009/10/09", "$1,200,000"],
+			// 			["Gavin Joyce", "Developer", "Edinburgh", "8822", "2010/12/22", "$92,575"],
+			// 			["Jennifer Chang", "Regional Director", "Singapore", "9239", "2010/11/14", "$357,650"],
+			// 			["Brenden Wagner", "Software Engineer", "San Francisco", "1314", "2011/06/07", "$206,850"],
+			// 			["Fiona Green", "Chief Operating Officer (COO)", "San Francisco", "2947", "2010/03/11", "$850,000"],
+			// 			["Shou Itou", "Regional Marketing", "Tokyo", "8899", "2011/08/14", "$163,000"],
+			// 			["Michelle House", "Integration Specialist", "Sidney", "2769", "2011/06/02", "$95,400"],
+			// 			["Suki Burks", "Developer", "London", "6832", "2009/10/22", "$114,500"],
+			// 			["Prescott Bartlett", "Technical Author", "London", "3606", "2011/05/07", "$145,000"],
+			// 			["Gavin Cortez", "Team Leader", "San Francisco", "2860", "2008/10/26", "$235,500"],
+			// 			["Martena Mccray", "Post-Sales support", "Edinburgh", "8240", "2011/03/09", "$324,050"],
+			// 			["Unity Butler", "Marketing Designer", "San Francisco", "5384", "2009/12/09", "$85,675"]
+			// 		];
+					
+			// 		db.close();
+			// 		response.end(JSON.stringify(dataSet));
+			// });
+			
+			app.post('/db/post', function(request, response) {
+				//console.log(request);
+				console.log(request);
+			    collection.insertOne(
+			    	{
+			    		name:		request.body.name,
+			    		position:	request.body.position,
+			    		location:	request.body.location,
+			    		lnumber:	request.body.lnumber,
+			    		graddate:	request.body.graddate,
+			    		salary:		'$' + request.body.salary
+			    	}
+			    );
+			    
+			    response.end();
+			});
+
 });
 
 
-		.post('/students', (req, res) => {
-			console.log('Posted Data: ' + JSON.stringify(req.body));
-			db.collection('students').insert(req.body, (err, data) => {
-				if (err) return console.log(err);
-				res.json(('Data saved: ' + JSON.stringify(data)));
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			/*
+			MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+				if (err) throw err;
+				const db = client.db('gradtracker');
+				app.get('/students', (req, res) => {
+
+					db.collection('students').find().toArray((err, result) => {
+						if (err) throw err;
+						console.log(result);
+						res.json(result);
+					});
+
+				})
 			});
-		})
-
-		.put((req, res) => {})
-		.delete(('/students', (req, res) => {
 
 
+					.post('/students', (req, res) => {
+						console.log('Posted Data: ' + JSON.stringify(req.body));
+						db.collection('students').insert(req.body, (err, data) => {
+							if (err) return console.log(err);
+							res.json(('Data saved: ' + JSON.stringify(data)));
+
+						});
+					})
+
+					.put((req, res) => {})
+					.delete(('/students', (req, res) => {
 
 
-		}));
-*/
+
+
+					}));
+			*/
